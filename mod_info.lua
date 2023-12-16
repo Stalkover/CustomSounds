a46f9179f5b0265cedd13f753790074f97c69cdb
Stalkover <armankarimov24@gmail.com>
2023-12-16 12:06:48 +0600

    second commit

--- mod_info.lua
+++ mod_info.lua
@@ -1,7 +1,7 @@
 name = "Custom Sounds"
 uid = "9c263f41-a12c-4485-995f-97f147b7c3b8"
 version = 1.0
-description = "Adds funny custom sounds"
+description = "Adds custom sounds"
 author = "Stalkover"
 exclusive = false
 ui_only = true
